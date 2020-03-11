6934ea757f25ebd33d9f3e40103ccaea32911af1
LittleDevilBig <47845786+LittleDevilBig@users.noreply.github.com>
2020-03-11 16:45:57 +0800

    Add files via upload

--- ex1/multiple variables.py
+++ ex1/multiple variables.py
@@ -1,49 +1,67 @@
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
+from numpy import loadtxt
 
-path = r'ex1data2.txt' #导入数据集
-data = pd.read_csv(path,names = ['sizes','rooms','prices'])
+data = loadtxt('ex1data2.txt', delimiter=',')
 
-data['sizes'] = (data['sizes'] - data['sizes'].mean())/data['sizes'].std() #特征缩放
-data['rooms'] = (data['rooms'] - data['rooms'].mean())/data['rooms'].std()
-data['prices'] = (data['prices'] - data['prices'].mean())/data['prices'].std()
+X = data[:, :-1]
+#最后一列为label
+y = data[:, -1:]
 
-theta = np.matrix(np.zeros(3))
-#插入一列，作为x0,值为1
-data.insert(0,'ones',1)
-cols = data.shape[1]
-X = np.matrix(data.iloc[:,0:cols-1])
-Y = np.matrix(data.iloc[:,cols-1:cols])
-#代价函数
-def computeCost(X,Y,theta):
-    m = X.shape[0]
-    return np.sum(np.power((X*theta.T) - Y,2))/(2*m)
+def feature_normalization(X):
+    X_norm = X
+    column_mean = np.mean(X_norm, axis=0)
+    column_std = np.std(X_norm, axis=0)
+    X_norm = X_norm - column_mean
+    X_norm = X_norm / column_std
+    return column_mean, column_std, X_norm
+means, stds, X_norm = feature_normalization(X)
+
+m = len(y)
+X_norm = np.hstack((np.ones((m,1)), X_norm))
+theta = np.zeros((X_norm.shape[1], 1))
+
+def computeCost(X, y, theta):
+    h = np.dot(X, theta)
+    J = 1/(2*m) * sum((h-y)**2)
+    return J[0]
+computeCost(X_norm, y, theta)
+
+alpha = 0.01;
+iterations = 400;
 #梯度下降
-def gradientDescent(X,Y,theta,alpha,epoch):
-    m = X.shape[0]
-    cost = []
-    for i in range(epoch):
-        theta = theta - alpha/m*(X*theta.T - Y).T*X
-        cost.append(computeCost(X,Y,theta))       #列表添加元素
-    return theta,cost
+def gradientDescent(X, y, theta, alpha, iterations):
+     J_history = np.zeros((iterations, 1))
+     for i in range(iterations):
+        h = np.dot(X, theta)
+        k = np.dot(np.transpose(X) ,(h-y))
+        theta = theta - alpha * k / m
+        J_history[i] = computeCost(X, y, theta)
+     return theta, J_history
+theta, J_history= gradientDescent(X_norm, y, theta, alpha, iterations)
+print(theta)
 
-alpha = 0.01
-epoch = 1000
-final_theta,cost = gradientDescent(X,Y,theta,alpha,epoch)
-final_cost = computeCost(X,Y,final_theta)
-print(final_theta)
-
-#画图查看训练过程
-fig,ax = plt.subplots(figsize=(12,8))
-ax.plot(np.arange(epoch),cost,'r')
-ax.set_xlabel('Iterations')
-ax.set_ylabel('Cost')
-ax.set_title('Error vs. Training Epoch')
-plt.show()
-
-#正规方程法
+# 正规方程
 def normalEqn(X, y):
-    theta = np.linalg.inv(X.T*X)*X.T*y
-    return theta.T
-print(final_theta)
+    theta = np.linalg.inv(X.T@X)@X.T@y#X.T@X等价于X.T.dot(X)
+    return theta
+theta2=normalEqn(X_norm, y)
+print(theta2)
+
+def plotJ(J_history, iterations):
+    x = np.arange(1, iterations+1)
+    plt.plot(x, J_history)
+    plt.xlabel('iterations')
+    plt.ylabel('loss')
+    plt.title('iterations vs loss')
+    plt.show()
+plotJ(J_history, iterations)
+
+def test(means, stds, theta):
+    t1 = np.array([[1650,3]])
+    t1 = t1 - means
+    t1 = t1 / stds
+    t1 = np.hstack((np.ones((t1.shape[0], 1)), t1))
+    res = np.dot(t1, theta)
+    print('predict house price:', res[0][0])
+test(means, stds, theta2)
