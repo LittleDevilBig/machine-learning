11b79a44e138321c7d159840dc3f3354decca048
LittleDevilBig <47845786+LittleDevilBig@users.noreply.github.com>
2020-05-17 09:34:24 +0800

    Add files via upload

---
+++ ex7/kmeans.py
@@ -0,0 +1,81 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sb
+from scipy.io import loadmat
+from IPython.display import Image
+
+def find_closest_centroids(X, centroids):
+    m = X.shape[0]
+    k = centroids.shape[0]
+    idx = np.zeros(m)
+    for i in range(m):
+        min_dist = 1000000
+        for j in range(k):
+            dist = np.sum((X[i, :] - centroids[j, :]) ** 2)
+            if dist < min_dist:
+                min_dist = dist
+                idx[i] = j
+    return idx
+data = loadmat('ex7data2.mat')
+X = data['X']
+initial_centroids = initial_centroids = np.array([[3, 3], [6, 2], [8, 5]])
+idx = find_closest_centroids(X, initial_centroids)
+print(idx[0:3])
+
+def compute_centroids(X, idx, k):
+    m, n = X.shape
+    centroids = np.zeros((k, n))
+    for i in range(k):
+        indices = np.where(idx == i)
+        centroids[i, :] = (np.sum(X[indices, :], axis=1) / len(indices[0])).ravel()
+    return centroids
+print(compute_centroids(X, idx, 3))
+
+def run_k_means(X, initial_centroids, max_iters):
+    m, n = X.shape
+    k = initial_centroids.shape[0]
+    idx = np.zeros(m)
+    centroids = initial_centroids
+    for i in range(max_iters):
+        idx = find_closest_centroids(X, centroids)
+        centroids = compute_centroids(X, idx, k)
+    return idx, centroids
+idx, centroids = run_k_means(X, initial_centroids, 10)
+cluster1 = X[np.where(idx == 0)[0],:]
+cluster2 = X[np.where(idx == 1)[0],:]
+cluster3 = X[np.where(idx == 2)[0],:]
+fig, ax = plt.subplots(figsize=(12,8))
+ax.scatter(cluster1[:,0], cluster1[:,1], s=30, color='r', label='Cluster 1')
+ax.scatter(cluster2[:,0], cluster2[:,1], s=30, color='g', label='Cluster 2')
+ax.scatter(cluster3[:,0], cluster3[:,1], s=30, color='b', label='Cluster 3')
+ax.legend()
+plt.show()
+
+def init_centroids(X, k):
+    m, n = X.shape
+    centroids = np.zeros((k, n))
+    idx = np.random.randint(0, m, k)
+    for i in range(k):
+        centroids[i, :] = X[idx[i], :]
+    return centroids
+
+Image(filename='bird_small.png')
+image_data = loadmat('bird_small.mat')
+A = image_data['A']
+# normalize value ranges
+A = A / 255.
+# reshape the array
+X = np.reshape(A, (A.shape[0] * A.shape[1], A.shape[2]))
+# randomly initialize the centroids
+initial_centroids = init_centroids(X, 16)
+# run the algorithm
+idx, centroids = run_k_means(X, initial_centroids, 10)
+# get the closest centroids one last time
+idx = find_closest_centroids(X, centroids)
+# map each pixel to the centroid value
+X_recovered = centroids[idx.astype(int),:]
+# reshape to the original dimensions
+X_recovered = np.reshape(X_recovered, (A.shape[0], A.shape[1], A.shape[2]))
+plt.imshow(X_recovered)
+plt.show()
